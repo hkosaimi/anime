8700f5062b516a10e03d35962a8b66c9dc939004
Hussain <hkosaimi@gmail.com>
2024-04-17 22:40:16 +0300

    gallary

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { motion } from "framer-motion";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.jpg";
+
+function Image({ title, id }) {
+  return (
+    <>
+      <motion.div
+        className="gallary"
+        style={{ position: "relative" }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}>
+        <img src={id} />
+        <h1>{title}</h1>
+      </motion.div>
+    </>
+  );
+}
 function Home() {
   return (
     <>
       <div className="home">
-        <motion.h1 initial={{ x: "-300px" }} animate={{ x: "100px" }}>
-          Photography is passion
-        </motion.h1>
+        <div className="img-container">
+          <Image title="Vitrupe, Limbaži Municipality, Latvia" id={img4} />
+          <Image title="Standing Man Beside Camel on Desert" id={img2} />
+          <Image title="Türkiye" id={img3} />
+        </div>
       </div>
     </>
   );
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,6 @@
 import { motion, useTransform, useScroll, useMotionValueEvent, useAnimate } from "framer-motion";
 import { useRef, useState } from "react";
+import { navigate } from "react-router-dom";
 
 function Test() {
   const [show, setShow] = useState(true);
@@ -35,65 +36,69 @@ function Test() {
   console.log(xVariants.visible);
   return (
     <>
-      <motion.div
-        className="temp"
-        variants={show ? xVariants : exitVariants}
-        animate="visible"
-        initial="hidden"
-        style={{ marginTop: "200px" }}>
-        {title.map((x) => (
-          <motion.p variants={xVariants}>{x}</motion.p>
-        ))}
-      </motion.div>
-      <motion.div
-        variants={!show && exitVariants}
-        animate="visible"
-        initial="hidden"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          columnGap: "10px",
-          marginTop: "20px",
-          alignItems: "center",
-          justifyContent: "center",
-        }}>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1,
-            transition: {
-              repeat: Infinity,
-              duration: 1,
-            },
-          }}
-          style={{
-            width: "10px",
-            height: "10px",
-            background: "green",
-            borderRadius: "50%",
-          }}></motion.div>
-        <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24">
-          <motion.path
-            initial={{ pathLength: 0 }}
-            animate={{
-              pathLength: 1,
-              transition: { duration: 3, repeat: Infinity },
-            }}
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M14 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Zm7 11-6-2V9l6-2v10Z"
-          />
-        </svg>
-      </motion.div>
+      {show && (
+        <>
+          <motion.div
+            className="temp"
+            variants={show ? xVariants : exitVariants}
+            animate="visible"
+            initial="hidden"
+            style={{ marginTop: "200px" }}>
+            {title.map((x) => (
+              <motion.p variants={xVariants}>{x}</motion.p>
+            ))}
+          </motion.div>
+          <motion.div
+            variants={!show && exitVariants}
+            animate="visible"
+            initial="hidden"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              columnGap: "10px",
+              marginTop: "20px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  repeat: Infinity,
+                  duration: 1,
+                },
+              }}
+              style={{
+                width: "10px",
+                height: "10px",
+                background: "green",
+                borderRadius: "50%",
+              }}></motion.div>
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24">
+              <motion.path
+                initial={{ pathLength: 0 }}
+                animate={{
+                  pathLength: 1,
+                  transition: { duration: 3, repeat: Infinity },
+                }}
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Zm7 11-6-2V9l6-2v10Z"
+              />
+            </svg>
+          </motion.div>
+        </>
+      )}
     </>
   );
 }
